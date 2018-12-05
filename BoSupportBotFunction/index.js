d2e123ca42b37480707a77e7f7cf88f0f06135c8
Anton Leliuk <anton.leluk@gmail.com>
2018-12-05 22:25:39 +0200

    Azure function

--- BoSupportBotFunction/index.js
+++ BoSupportBotFunction/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const azure_functions_ts_essentials_1 = require("azure-functions-ts-essentials");
 function run(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res.body = 'Hello you!';
+    context.res.status = azure_functions_ts_essentials_1.HttpStatusCode.OK;
+    context.done(null, {});
 }
 exports.run = run;
--- BoSupportBotFunction/index.ts
+++ BoSupportBotFunction/index.ts
@@ -1,7 +1,10 @@
-import { Context, HttpRequest } from 'azure-functions-ts-essentials'
+import { Context, HttpRequest, HttpStatusCode } from 'azure-functions-ts-essentials'
 
 export function run (context : Context, req : HttpRequest) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.res.body = 'Hello you!'
+    context.res.body = 'Hello you!';
+    context.res.status = HttpStatusCode.OK;
+
+    context.done(null, {});
 }
